d03875681a4b4facaad5951fa47b064f9e61938d
NitinNanda <nitinnanda11@gmail.com>
2020-07-12 14:37:23 -0400

    added tab panes for Login and register pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,6 +12,9 @@ const routes = [
       },
       { path: '/settings/help', 
       component: () => import('pages/PageHelp.vue') 
+      },
+      { path: '/auth', 
+      component: () => import('pages/PageAuth.vue') 
       }
     ]
   }
