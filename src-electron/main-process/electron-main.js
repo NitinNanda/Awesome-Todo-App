269b1549d78df1d4cd665a6d3053fa2a6820f660
NitinNanda <nitinnanda11@gmail.com>
2020-07-17 00:15:57 -0400

    Setting up for production on Mac

---
+++ src-electron/main-process/electron-main.js
@@ -0,0 +1,55 @@
+import { app, BrowserWindow, nativeTheme, Menu } from 'electron'
+import { template } from './electron-main-menu-template'
+
+try {
+  if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
+    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+  }
+} catch (_) { }
+
+/**
+ * Set `__statics` path to static files in production;
+ * The reason we are setting it here is that the path needs to be evaluated at runtime
+ */
+if (process.env.PROD) {
+  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\')
+}
+
+/*
+ Variables and costants
+*/
+let mainWindow
+const menu = Menu.buildFromTemplate(template)
+/*
+App ready 
+*/
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    minWidth: 800,
+    minHeight: 500,
+    useContentSize: true,
+    webPreferences: {
+      nodeIntegration: QUASAR_NODE_INTEGRATION,
+      nodeIntegrationInWorker: QUASAR_NODE_INTEGRATION,
+    }
+  })
+
+  mainWindow.loadURL(process.env.APP_URL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+  Menu.setApplicationMenu(menu)
+})
+
+/*
+App events
+*/
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
